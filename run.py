2df01c636c76d7fd1f61c1e653821bc5b82bd727
Mark Baggett <mbagget1@utk.edu>
2020-12-29 09:26:01 -0500

    Add optional server name.

--- run.py
+++ run.py
@@ -4,6 +4,13 @@ from iiif.manifest import Manifest
 import argparse
 import json
 
+
+def cleanup_server_name(server_uri):
+    if server_uri.endswith("/"):
+        server_uri = server_uri[:-1]
+    return server_uri.replace("/collections")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate Manifest from a UTK Book")
     parser.add_argument(
@@ -20,6 +27,13 @@ if __name__ == "__main__":
         help="Specify a filename. Defaults to manifest.json.",
         default="manifest.json",
     )
+    parser.add_argument(
+        "-s",
+        "--server",
+        dest="filename",
+        help="Specify a server.  This is the value that will be used for harvesting metadata and writing id values in the manifest.",
+        default="https://digital.lib.utk.edu",
+    )
     args = parser.parse_args()
     book_pages = TuplesSearch(language="sparql").get_pages_and_page_numbers(
         args.book_pid
@@ -27,7 +41,14 @@ if __name__ == "__main__":
     collection_pid = TuplesSearch(language="sparql").get_parent_collection(
         args.book_pid
     )
-    metadata = MODSScraper(args.book_pid).build_iiif_descriptive_metadata()
-    manifest_object = Manifest(metadata, book_pages, collection_pid)
+    metadata = MODSScraper(
+        args.book_pid, islandora_frontend=cleanup_server_name(args.server)
+    ).build_iiif_descriptive_metadata()
+    manifest_object = Manifest(
+        metadata,
+        book_pages,
+        collection_pid,
+        server_uri=cleanup_server_name(args.server),
+    )
     with open(args.filename, "w") as manifest:
         manifest.write(json.dumps(manifest_object.manifest, indent=4))
