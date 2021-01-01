298c0b221d4f0fee950432f83eef740b7ff28549
Mark Baggett <mbagget1@utk.edu>
2021-01-01 11:50:18 -0500

    Begin refactor to make app content model agnostic.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from fedora.mods import MODSScraper
 from fedora.risearch import TuplesSearch
 from iiif.manifest import Manifest
+from iiif.presentation3 import Manifest3
 import argparse
 import json
 
@@ -14,9 +15,9 @@ def cleanup_server_name(server_uri):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate Manifest from a UTK Book")
     parser.add_argument(
-        "-b",
-        "--book",
-        dest="book_pid",
+        "-p",
+        "--pid",
+        dest="pid",
         help="Specify the pid of the book you want to base your manifest on.",
         required=True,
     )
@@ -42,21 +43,37 @@ if __name__ == "__main__":
         default="http://localhost:8080/fedora/risearch",
     )
     args = parser.parse_args()
-    book_pages = TuplesSearch(
-        language="sparql", ri_endpoint=args.risearch
-    ).get_pages_and_page_numbers(args.book_pid)
-    collection_pid = TuplesSearch(
-        language="sparql", ri_endpoint=args.risearch
-    ).get_parent_collection(args.book_pid)
-    metadata = MODSScraper(
-        args.book_pid,
-        islandora_frontend=f"{cleanup_server_name(args.server)}/collections/",
-    ).build_iiif_descriptive_metadata_v2()
-    manifest_object = Manifest(
-        metadata,
-        book_pages,
-        collection_pid,
-        server_uri=f"{cleanup_server_name(args.server)}/",
-    )
-    with open(args.filename, "w") as manifest:
-        manifest.write(json.dumps(manifest_object.manifest, indent=4))
+    collection_and_model = TuplesSearch(
+        language="sparql", ri_endpoint=args.ri_endpoint
+    ).get_collection_and_content_model(args.pid)
+    supported_content_models: ("islandora:bookCModel", "islandora:sp-audioCModel")
+    if collection_and_model[1] == "islandora:bookCModel":
+        book_pages = TuplesSearch(
+            language="sparql", ri_endpoint=args.risearch
+        ).get_pages_and_page_numbers(args.pid)
+        metadata = MODSScraper(
+            args.pid,
+            islandora_frontend=f"{cleanup_server_name(args.server)}/collections/",
+        ).build_iiif_descriptive_metadata_v2()
+        manifest_object = Manifest(
+            metadata,
+            book_pages,
+            collection_and_model[0],
+            server_uri=f"{cleanup_server_name(args.server)}/",
+        )
+        with open(args.filename, "w") as manifest:
+            manifest.write(json.dumps(manifest_object.manifest, indent=4))
+    elif collection_and_model[1] == "islandora:sp-audioCModel":
+        metadata = MODSScraper(
+            args.pid,
+            islandora_frontend=f"{cleanup_server_name(args.server)}/collections/",
+        ).build_iiif_descriptive_metadata_v3()
+        manifest_object = Manifest3(
+            metadata, server_uri=f"{cleanup_server_name(args.server)}/"
+        ).build_audio_manifest()
+        with open(args.filename, "w") as manifest:
+            manifest.write(manifest_object)
+    else:
+        raise Exception(
+            f"Cannot generate manifests for {collection_and_model[1]} yet. Supported content models include: {supported_content_models}."
+        )
